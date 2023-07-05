49873a4f0fa49fe6553faf45c5ee5890b782602b
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-05 14:47:08 +0330

    styling Header section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
     <>
       <Header />
 
-      <Router>
+      {/* <Router>
         <Route path="/" element={Home} />
         <Route path="/products" element={Products} />
         <Route path="/about" element={About} />
         <Route path="/contact" element={Contact} />
-      </Router>
+      </Router> */}
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,20 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
   return (
     <>
-      <div class="bg-slate-950 h-24 flex justify-between">
-        <img
-          src="../src/assets/images/billig-book.png"
-          class="bg-white"
-          alt="billigbook"
-        ></img>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+      <div class="bg-slate-950 h-24 flex justify-between align-middle px-20 py-5">
+        <div class="w-1/2">
+          <img
+            src="../src/assets/images/billigbook.png"
+            class="h-full"
+            alt="billigbook"
+          ></img>
+        </div>
+        <div class="text-white w-1/3 flex justify-around items-center text-lg">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/products">Products</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
       </div>
     </>
   );
