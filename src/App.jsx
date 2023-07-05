d715ffdd94a0fd49feb4680ea679d75adea1ec54
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-05 15:36:18 +0330

    add Footer component and fix synt. error in Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
-import { Route, Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <Header />
-      <Home />
-      {/* <Router>
-        <Route path="/" element={Home} />
-        <Route path="/products" element={Products} />
-        <Route path="/about" element={About} />
-        <Route path="/contact" element={Contact} />
-      </Router> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+export const Footer = () => (
+  <div>
+    <div class="flex justify-around px-50 "></div>
+    <p>© 2023 Billigbook.</p>
+  </div>
+);
