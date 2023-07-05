36dcda1c5d63a0dfc8f8dcc68b33e830635bb9b9
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-05 15:40:45 +0330

    fixed Bug in Footer component synt.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
-export const Footer = () => (
-  <div>
-    <div class="flex justify-around px-50 "></div>
-    <p>© 2023 Billigbook.</p>
-  </div>
-);
+export const Footer = () => {
+  return (
+    <div>
+      <div class="flex justify-around px-50 "></div>
+      <p class="text-white">© 2023 Billigbook.</p>
+    </div>
+  );
+};
