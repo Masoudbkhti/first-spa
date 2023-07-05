f74a184b52f5846a2ea682d8f83660f1281ee227
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-05 15:23:17 +0330

    add Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 import { Route, Router } from "react-router-dom";
 function App() {
   return (
     <>
       <Header />
-
+      <Home />
       {/* <Router>
         <Route path="/" element={Home} />
         <Route path="/products" element={Products} />
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <img src="../src/assets/images/main.jpg"></img>
+    </div>
+  );
+};
