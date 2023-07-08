5ee3e9c48f1b7e6439756984b464854d94bf9374
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-08 11:07:41 +0330

    add Ttile and text and button to Home component - finish

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,26 @@
+import mainImage from "../assets/images/main.jpg";
+
 export const Home = () => {
+  const backgroundImage = `url(${mainImage})`;
+
   return (
-    <div>
-      <img src="../src/assets/images/main.jpg"></img>
+    <div
+      style={{ backgroundImage }}
+      className="bg-cover h-screen flex flex-col justify-center gap-8 pl-20"
+    >
+      <div className="bg-gray-400 bg-opacity-50 w-fit rounded-md p-10">
+        <h1 className="text-black lg:text-7xl sm:text-5xl font-semibold">
+          Send books abroad
+        </h1>
+        <p className="text-gray-950 mt-10 lg:text-3xl sm:text-xl">
+          Buy books from unlimited collection
+        </p>
+      </div>
+      <div>
+        <button className="rounded-lg border-gray-500 border border-solid bg-violet-300 px-5 py-2 ml-10">
+          Buy books
+        </button>
+      </div>
     </div>
   );
 };
