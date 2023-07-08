9b2347887c4b173529c36e8d5e132cb1f05a3df8
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-08 15:01:43 +0330

    fixed bug - logo url on vercel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,11 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/images/billigbook.png";
 export const Header = () => {
   return (
     <>
       <div class="bg-slate-950 h-24 flex justify-between align-middle px-20 py-5">
         <div class="w-1/2">
-          <img
-            src="../src/assets/images/billigbook.png"
-            class="h-full"
-            alt="billigbook"
-          ></img>
+          <img src={logo} class="h-full" alt="billigbook"></img>
         </div>
         <div class="text-white w-1/3 flex justify-around items-center text-lg">
           <NavLink to="/" activeClassName="font-bold">
