a34ffa477695246b435bb622424cdb802ecdc4f2
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-08 12:25:25 +0330

    complete Products

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import { useParams } from "react-router-dom";
 export const Card = ({ img, title, price }) => {
   const params = useParams();
   return (
-    <div className="rounded-md overflow-hidden">
-      <img src={img} className="w-20" />
-      <h1>
+    <div className="rounded-md overflow-hidden shadow-lg py-10 px-5 flex flex-col gap-5">
+      <img src={img} className="w-auto" />
+      <h1 className="text-2xl font-bold">
         {title}
         {params.id}
       </h1>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,27 +2,48 @@ import { Link } from "react-router-dom";
 import { Card } from "./Card";
 export const Products = () => {
   return (
-    <div className="py-20 flex justify-center">
-      <div className="grid grid-cols-3 gap-80">
+    <div className="py-20 px-20 flex justify-center">
+      <div className="grid grid-cols-3 gap-40">
         <Link to="/products/1">
           <Card
             img="./src/assets/images/sizif.jpg"
-            title="Afsane Sizif"
-            price="20 Euro"
+            title="Der Mythos des Sisyphos"
+            price="18 Euro"
           />
         </Link>
         <Link to="/products/2">
           <Card
             img="./src/assets/images/ikigay.jpg"
-            title="Ikigay"
-            price="20 Euro"
+            title="Ikigai"
+            price="25 Euro"
           />
         </Link>
         <Link to="/products/3">
           <Card
-            img="./src/assets/images/sizif.jpg"
-            title="Afsane sizif"
-            price="20 Euro"
+            img="./src/assets/images/ablah.jpg"
+            title="Der Idiot"
+            price="29 Euro"
+          />
+        </Link>
+        <Link to="/products/3">
+          <Card
+            img="./src/assets/images/ansuyemarg.jpg"
+            title="Jenseits des todes"
+            price="19 Euro"
+          />
+        </Link>
+        <Link to="/products/3">
+          <Card
+            img="./src/assets/images/mana.jpg"
+            title="Ja zum Leben"
+            price="39 Euro"
+          />
+        </Link>
+        <Link to="/products/3">
+          <Card
+            img="./src/assets/images/badbadakbaz.jpg"
+            title="Drachenläufer"
+            price="49 Euro"
           />
         </Link>
       </div>
