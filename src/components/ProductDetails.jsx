4a092e7170876589f2923118e923aa95cd056387
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-09 11:54:08 +0330

    add description and sliderimg to productpages

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -4,13 +4,15 @@ import { useParams } from "react-router";
 export const ProductDetails = ({ products }) => {
   let { id } = useParams();
   const product = products[id];
-  const { title, img, price } = product;
+  const { title, img, price, sliderimg, description } = product;
 
   return (
-    <div className="rounded-md overflow-hidden shadow-lg py-10 px-5 flex flex-col gap-5">
-      <img src={img} className="w-auto" />
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <p>{price}</p>
+    <div>
+      <img src={sliderimg} className="w-screen" />
+      <div class="flex flex-col gap-5 justify-center items-center py-10 px-10">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 
 export const Products = ({ products }) => {
   return (
-    <div className="py-20 px-20 flex justify-center">
-      <div className="grid grid-cols-3 gap-40">
+    <div class="py-20 px-20 flex justify-center">
+      <div class="grid grid-cols-3 gap-40">
         {Object.entries(products).map(([id, { title, img, price }]) => (
           <div
             key={id}
-            className="rounded-md overflow-hidden shadow-lg py-10 px-5 flex flex-col gap-5"
+            class="rounded-md overflow-hidden shadow-lg py-10 px-5 flex flex-col gap-5"
           >
             <Link to={`/products/${id}`}>
-              <img src={img} className="w-auto" />
-              <h1 className="text-2xl font-bold">{title} </h1>
-              <p>{price}</p>
+              <img src={img} class="w-auto" />
+              <h1 class="text-2xl font-bold mt-5">{title} </h1>
             </Link>
+            <p>{price}</p>
           </div>
         ))}
       </div>
