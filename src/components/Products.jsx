9fbfb7b72c77f85498e9bf8a4c6a1416417ceb2e
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-09 16:12:49 +0330

    fetch data from db.json in Products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,23 +4,30 @@ import { useEffect, useState } from "react";
 const PRODUCT_LIST_API = "http://localhost:3000/products";
 
 export const Products = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [productDetails, setProductDetails] = useState([]);
 
   useEffect(() => {
-    fetch(PRODUCT_LIST_API)
-      .then((data) => {
-        setProductDetails(data.results);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const rawData = await fetch(PRODUCT_LIST_API);
+        const data = await rawData.json();
+        setIsLoading(false);
+        setProductDetails(data);
+      } catch (e) {
+        setIsLoading(false);
+      }
+    };
+    getData();
   }, []);
 
   return (
     <div class="py-20 px-36 flex flex-col justify-center items-center gap-10">
       <h2 class="text-5xl font-bold">Our Books</h2>
+      {isLoading ? <div>Loading...</div> : null}
       <div class="grid grid-cols-3 gap-20">
-        {productDetails.map(([id, { title, img, price }]) => (
+        {productDetails.map(({ id, title, img, price }) => (
           <div
             key={id}
             class="rounded-md overflow-hidden shadow-lg py-10 px-5 flex flex-col gap-5"
