bdb0dc07bbb70d501013c2432c05c16bf8d44651
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-09 12:56:12 +0330

    complete Contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,39 @@
 import contactimg from "../assets/images/contact.jpg";
 export const Contact = () => {
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <div class="grid grid-cols-2">
       <img src={contactimg} class="w-screen"></img>
       <div class="py-20 px-36 flex flex-col justify-center items-center gap-10">
         <h2 class="text-5xl font-bold">Contact Us</h2>
-        <p>
-          One of the concerns of Iranians living abroad has always been the
-          preparation of Iranian books; Because access to stores that sell
-          Iranian books is very limited and usually the desired book can not be
-          found there. The Billigbook has solved this long-standing problem, and
-          from now on you can easily buy books from Iran anywhere on the planet
-          and have your book delivered to your door as soon as possible.
-        </p>
+        <form class="flex flex-col gap-3 w-full" onSubmit={formSubmitHandler}>
+          <label for="name">Full Name</label>
+          <input
+            for="name"
+            type="text"
+            placeholder="Enter your name..."
+            class="placeholder:text-black font-semibold border-b border-gray-500 py-2 focus:outline-none"
+          ></input>
+          <label for="name">Email</label>
+          <input
+            for="name"
+            type="text"
+            placeholder="Enter Email..."
+            class="placeholder:text-black font-semibold border-b border-gray-500 py-2 focus:outline-none"
+          ></input>
+          <label for="name">Message</label>
+          <input
+            for="name"
+            type="textarea"
+            placeholder="Enter Message..."
+            class="placeholder:text-black font-semibold border-b border-gray-500 py-2 focus:outline-none"
+          ></input>
+          <button className=" bg-violet-700 hover:bg-violet-500 text-white font-bold py-2 px-4 rounded mt-10 w-1/3">
+            Send Message
+          </button>
+        </form>
       </div>
     </div>
   );
